992552460ba8226eaaaf406777c005d730554ae2
김진석 <@DESKTOP-25BBJS4>
2018-10-10 22:16:47 +0900

    인터셉터 Mapping 수정

--- spring02/src/main/java/com/example/spring02/interceptor/AdminInterceptor.java
+++ spring02/src/main/java/com/example/spring02/interceptor/AdminInterceptor.java
@@ -4,14 +4,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class AdminInterceptor extends HandlerInterceptorAdapter{
 	
+	private static final Logger logger = 
+			LoggerFactory.getLogger(AdminInterceptor.class);
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		logger.info("Admin preHandle");
+		
 		HttpSession session = request.getSession();
 		
 		if(session.getAttribute("admin_userid")==null) {
@@ -23,10 +30,5 @@ public class AdminInterceptor extends HandlerInterceptorAdapter{
 			return true;
 		}
 	}
-	
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		super.postHandle(request, response, handler, modelAndView);
-	}
+
 }
